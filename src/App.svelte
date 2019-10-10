9a5a33660ff6ab1bf722d3649d110d6c528b572d
Emrullah Ayın <emrullah.ayin@otelz.com>
2019-10-10 10:46:13 +0300

    add button

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-	export let name;
+  let count = 0;
+  export function handleClick() {
+    count += 1;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  button {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<button on:click={handleClick}>Clicked {count}</button>
